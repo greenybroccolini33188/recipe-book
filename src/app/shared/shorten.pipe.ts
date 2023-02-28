2b35edc1434c45a5161344a7f0b9d6f293e3b3d3
Brock Glatman <bglatman@outlook.com>
2023-02-27 18:48:17 -0800

    fix pipes

--- src/app/shared/shorten.pipe.ts
+++ src/app/shared/shorten.pipe.ts
@@ -4,8 +4,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'shorten',
 })
 export class ShortenPipe implements PipeTransform {
-  public transform(value: string, length: number = 10): string {
-    if (value.length > 10) return `${value.slice(0, length)} ...`;
+  public transform(value: string, limit: number = 10): string {
+    if (value.length > 10) return `${value.slice(0, limit)} ...`;
     return value;
   }
 }
