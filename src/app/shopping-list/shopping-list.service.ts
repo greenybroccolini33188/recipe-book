7249e2d1c606c2eba3deb8bc7a7b16154fccd146
Brock Glatman <bglatman@outlook.com>
2023-02-27 20:29:43 -0800

    update imports

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { Ingredients } from '../shared/typings';
 import { Subject } from 'rxjs';
