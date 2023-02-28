1ebb1f01156047aba7bb8a764e9d1dee56eaa945
Brock Glatman <bglatman@outlook.com>
2023-02-27 17:38:21 -0800

    remove redundant css

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-    `
-      .cursor-pointer {
-        cursor: pointer;
-      }
-    `,
-  ],
+  styles: [],
 })
 export class HeaderComponent implements OnInit {
   public collapsed: boolean;
